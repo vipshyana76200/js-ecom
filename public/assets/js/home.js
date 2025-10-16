a5a66b013f3dbb211d9ef1b56c02447da295f01e
vipshyana <vipshyanabelkhede@gmail.com>
2025-10-16 09:07:52 +0530

    backend url fixed

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,7 +1,8 @@
 import { showToast } from "./utils.js"
 
 const home = document.getElementById("home")
-const URL = "http://localhost:5000"
+const URL = "https://js-ecom-kuv9.onrender.com"
+// const URL = "http://localhost:5000"
 const count = document.getElementById("count")
 const productLimit = document.getElementById("product-limit")
 const pagi = document.getElementById("pagi")
